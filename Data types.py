44bededcf24e458540ad538d5ad51b72f1ca6464
JoshuaBidjou123 <kristen.bidjou@gmail.com>
2025-07-26 12:24:30 -0400

    Commit

---
+++ Data types.py
@@ -0,0 +1,20 @@
+name = "Penguin"
+age = 15
+is_student = True
+Weight = 38.5
+
+print("Name :", name)
+print("Data Type of name is", type(name))
+print("Age :", age)
+print("Data Type of Age is ",type(age))
+print("is_student :", is_student)
+print("data Type of is_student is ", type(is_student))
+print("Weight :", Weight)
+print("DAta Type of Weight is", type(Weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of Age is", type(age))
+weight = int(Weight)
+print("Data Type of Weight is", type(Weight) )
